8d4e1a76b9cfd69cbb18e532720cbbb1685f3e26
JYellow363 <jyellow363@gmail.com>
2018-04-23 15:38:17 -0500

    Create guardado.cpp

---
+++ guardado.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    cin>>n;
+    cout<<n;
+    return 0;
+}
